db44f7cec302d5e91528f7533f72a5cab8c7cc6c
Himanshu Bansal <himanshubansal155@gmail.com>
2022-02-28 15:43:36 +0530

    api call testing

--- src/server.js
+++ src/server.js
@@ -13,42 +13,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(process.env.PORT || 3000, () => {
   console.log("listening at port", process.env.PORT);
 });
-
-app.get("/", async (req, res) => {
-  try {
-    //   // const newUser = await new user({
-    //   //   // email: "himanshu@gmail.com",
-    //   //   // name: "himanshu",
-    //   //   // address: "lal",
-    //   // }).save();
-    //   // const pass = await new Passwords({
-    //   //   // userId: newUser._id,
-    //   //   // password: "123456",
-    //   // }).save();
-    user
-      .findOne({ email: "himanshubansal155gmail.com" })
-      .populate("password")
-      .exec(function (err, story) {
-        // if (err) return handleError(err);
-        console.log("The author is %s", err, story);
-        // prints "The author is Ian Fleming"
-        res.send(story);
-      });
-    console.log(newUser, pass);
-  } catch (err) {
-    // console.log(err);
-    res.send(err);
-  }
-  res.send("hello");
-});
-
-app.post("/user", async (req, res) => {
-  res.send(req.body);
-});
-
 const uri =
   "mongodb+srv://expense-tracker:9458494357@cluster0.8imxs.mongodb.net/Expense-tracker?retryWrites=true&w=majority";
 mongoose
   .connect(uri)
   .then((res) => console.log("DB connected"))
   .catch((err) => console.log("DB CONNECTION ERR", err));
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await user.findOne({ email: "himanshu@gmail.com" });
+    res.send(users);
+  } catch (err) {
+    // console.log(err);
+    res.send(err);
+  }
+});
+
+app.post("/user", async (req, res) => {
+  res.send(req.body);
+});
