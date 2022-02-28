af5c322b70f0e7fceeccbc9663cc7cb7d399b5bf
Himanshu Bansal <himanshubansal155@gmail.com>
2022-02-28 15:21:25 +0530

    port error checking

--- src/server.js
+++ src/server.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.NODE_PORT || 3000, () => {
-  console.log("listening at port", process.env.NODE_PORT);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening at port", process.env.PORT);
 });
 
 app.get("/", async (req, res) => {
